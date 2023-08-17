4da38e1cfae85d9d2d460c50d40a51e7b95e222d
rohit chand <rohitchand010904@gmail.com>
2023-08-17 09:26:37 +0530

    fdgbdf

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 console.log("welcome to spotify");
 let songindex=0;
-let audioElement = new Audio('1.mp3');
 let masterplay = document.getElementById('masterplay');
 let progressbar = document.getElementById('progressbar');
 let songs = [
     {songname: "WE ROLLIN", filepath:"1.mp3", coverpath:""},
-    {songname: "WE ROLLIN", filepath:"2.mp3", coverpath:""},
-    {songname: "WE ROLLIN", filepath:"3.mp3", coverpath:""},
-    {songname: "WE ROLLIN", filepath:"4.mp3", coverpath:""},
-    {songname: "WE ROLLIN", filepath:"5.mp3", coverpath:""},
-    {songname: "WE ROLLIN", filepath:"6.mp3", coverpath:""}
+    {songname: "HER", filepath:"2.mp3", coverpath:""},
+    {songname: "DIOR", filepath:"3.mp3", coverpath:""},
+    {songname: "NO LOVE", filepath:"4.mp3", coverpath:""},
+    {songname: "CHEQUES", filepath:"5.mp3", coverpath:""},
+    {songname: "STILL ROLLIN", filepath:"6.mp3", coverpath:""}
     
 ]
-audioElement.play();
+idx = Math.floor(Math.random()*6);
 
+let audioElement = new Audio(songs[idx].filepath);
+
+audioElement.play();
 function playPause() {
-    if(audioElement.paused || audioElement.currentTime<=0){
+    if(audioElement.paused){
         audioElement.play();
         masterplay.className = "fa-solid fa-pause fa-2xl";
        }
@@ -41,14 +43,28 @@ document.body.onkeydown = function(e) {
 
 function updateTime(){
     // console.log('timeupdate');
-    progress = parseInt((audioElement.currentTime/audioElement.duration)*10000);
-    // console.log('progress');
-    progressbar.value = progress;
-}
+    if (audioElement.paused){
 
-setInterval(updateTime, 100);
+    // }else {
+        progress = parseInt((audioElement.currentTime/audioElement.duration)*10000);
+        // console.log('progress');
+        progressbar.value = progress;
+        if (progressbar.value > 9700){
+            let audioElement = new Audio(songs[++idx].filepath);
+            audioElement.play();
+            progressbar.value = 0;
+        }
+        console.log(progress)
+    }
+    // console.log(progress);
+}
 progressbar.addEventListener('change',()=>{
-    audioElement.currentTime = progressbar.value*audioElement.duration/100;
+    audioElement.currentTime = progressbar.value*audioElement.duration/10000;
+    
+    console.log(progressbar.value)
+    
 })
 
 
+
+setInterval(updateTime, 10000);
